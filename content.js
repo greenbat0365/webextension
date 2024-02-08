88c96407af29306ca944a7d3af4248551f9b5baa
greenbat0365 <136758496+greenbat0365@users.noreply.github.com>
2024-02-08 16:32:40 -0600

    Add files via upload

--- content.js
+++ content.js
@@ -45,82 +45,82 @@ Line 2: Lion L&K #24,Company : BULLDOG LOCKSMITH (LINE-2),Uleadz Technician: Lin
 ",Hours of Operation: Ignition Jobs:  7 Days a Week from 7.00 AM - 11.00 PMOther Jobs: 24 hours a day 7 Days a Week,Can't Do List: No Can’t Do List ,"Notes: When you get a Non-Ignition Job i.e., Lock Change / Install Lock / Re-Key, House or Car Lockouts after Hours(11.00 PM to 7.00 AM)Book the Lead as usual but at the end of the call, tell the Customer: “Currently our Technician is on another Job. As soon as he Finishes the Job, he will Call you.”"
 Line 2: Lion L&K #25,Company : BULLDOG LOCKSMITH (LINE-2),Uleadz Technician: Line 2 : Bulldog Technician,,Service Area: No Service Area (No OOA Jobs),"Affiliates: None
 ",Hours of Operation: Ignition Jobs:  7 Days a Week from 7.00 AM - 11.00 PMOther Jobs: 24 hours a day 7 Days a Week,Can't Do List: No Can’t Do List ,"Notes: When you get a Non-Ignition Job i.e., Lock Change / Install Lock / Re-Key, House or Car Lockouts after Hours(11.00 PM to 7.00 AM)Book the Lead as usual but at the end of the call, tell the Customer: “Currently our Technician is on another Job. As soon as he Finishes the Job, he will Call you.”"
-Jimmy LS (Chicago),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.","Service Area: No Service Area (No OOA Jobs)
+Line 3: Jimmy LS (Chicago),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.","Service Area: No Service Area (No OOA Jobs)
 ","Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-Jimmy LS (Detroit),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
+Line 3: Jimmy LS (Detroit),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
 ","Service Area: No Service Area (No OOA Jobs)
 ","Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-24/7 LS (Tampa),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
+Line 3: 24/7 LS (Tampa),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
 ",Service Area: No Service Area (No OOA Jobs),"Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-24/7 Mobile LS (Tampa),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
+Line 3: 24/7 Mobile LS (Tampa),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
 ","Service Area: No Service Area (No OOA Jobs)
 ","Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-24/7 Mobile LS (Detroit),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
+Line 3: 24/7 Mobile LS (Detroit),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
 ","Service Area: No Service Area (No OOA Jobs)
 ","Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-24/7 Mobile LS (Cleavland),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
+Line 3: 24/7 Mobile LS (Cleavland),Company : Jimmy Locksmith (LINE-3),Uleadz Technician: Line 3 : Jimmy Locksmith Tech.,"Metro:  Detroit, MI. Chicago, IL. Cleavland, OH.  & Tampa, FL.
 ","Service Area: No Service Area (No OOA Jobs)
 ","Affiliates: None
 ",Office Hours: 7 Days a Week from 7:00 AM - 12:00 AM,,"Can't Do List: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Any Ignition Jobs on Cars Older than 1999"
-Makey LS,"Company : Makey LS (LINE-5)
+Line 5: Makey LS,"Company : Makey LS (LINE-5)
 ","Uleadz Technician: Line 5: Makey LS 
 ","Metro: Rockville, Marryland","Service Area: Maryland and D.C only
 ","Affiliates: None
 ",Hours of Operation:  8.00 AM - 8.00 PM : 7 Days a Week,"Can't Do List: Cannot Duplicate Keys for anything except Vehicle Ignition & Vehicle Doors/Trunk. Cannot Make a New Key for anything except Vehicles Ignition & Vehicle Doors/Trunk. No Key Duplicates Ignition Jobs for Cars Older than 1998 Fix/Change Car Locks Motorcycle / Motorbike Keys / Scooters German Cars: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Fix Car Alarms or Car Computer Resets No Jobs with Safes: Lockout, New Key, Fix, etc. Battery Changes on Car Keys, Cars or Garage Doors Make a Key for a Trailer Hitch Lock Does not do any None-Locksmithing Jobs No Garage Door Fix and Install Jobs",
-Recommended LS,"Company : Makey LS (LINE-5)
+Line 5:Recommended LS,"Company : Makey LS (LINE-5)
 ","Uleadz Technician: Line 5: Makey LS 
 ","Metro: Rockville, Marryland","Service Area: Maryland and D.C only
 ","Affiliates: None
 ",Hours of Operation:  8.00 AM - 8.00 PM : 7 Days a Week,"Can't Do List: Cannot Duplicate Keys for anything except Vehicle Ignition & Vehicle Doors/Trunk. Cannot Make a New Key for anything except Vehicles Ignition & Vehicle Doors/Trunk. No Key Duplicates Ignition Jobs for Cars Older than 1998 Fix/Change Car Locks Motorcycle / Motorbike Keys / Scooters German Cars: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Fix Car Alarms or Car Computer Resets No Jobs with Safes: Lockout, New Key, Fix, etc. Battery Changes on Car Keys, Cars or Garage Doors Make a Key for a Trailer Hitch Lock Does not do any None-Locksmithing Jobs No Garage Door Fix and Install Jobs",
-Ultimate LS,"Company : Makey LS (LINE-5)
+Line 5:Ultimate LS,"Company : Makey LS (LINE-5)
 ","Uleadz Technician: Line 5: Makey LS 
 ","Metro: Rockville, Marryland","Service Area: Maryland and D.C only
 ","Affiliates: None
 ",Hours of Operation:  8.00 AM - 8.00 PM : 7 Days a Week,"Can't Do List: Cannot Duplicate Keys for anything except Vehicle Ignition & Vehicle Doors/Trunk. Cannot Make a New Key for anything except Vehicles Ignition & Vehicle Doors/Trunk. No Key Duplicates Ignition Jobs for Cars Older than 1998 Fix/Change Car Locks Motorcycle / Motorbike Keys / Scooters German Cars: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Fix Car Alarms or Car Computer Resets No Jobs with Safes: Lockout, New Key, Fix, etc. Battery Changes on Car Keys, Cars or Garage Doors Make a Key for a Trailer Hitch Lock Does not do any None-Locksmithing Jobs No Garage Door Fix and Install Jobs",
-Montgomery LS,"Company : Makey LS (LINE-5)
+Line 5:Montgomery LS,"Company : Makey LS (LINE-5)
 ","Uleadz Technician: Line 5: Makey LS 
 ","Metro: Rockville, Marryland","Service Area: Maryland and D.C only
 ","Affiliates: None
 ",Hours of Operation:  8.00 AM - 8.00 PM : 7 Days a Week,"Can't Do List: Cannot Duplicate Keys for anything except Vehicle Ignition & Vehicle Doors/Trunk. Cannot Make a New Key for anything except Vehicles Ignition & Vehicle Doors/Trunk. No Key Duplicates Ignition Jobs for Cars Older than 1998 Fix/Change Car Locks Motorcycle / Motorbike Keys / Scooters German Cars: Volkswagen, Mercedes Benz, Audi, BMW, Porsche, Volvo and any other German Cars Fix Car Alarms or Car Computer Resets No Jobs with Safes: Lockout, New Key, Fix, etc. Battery Changes on Car Keys, Cars or Garage Doors Make a Key for a Trailer Hitch Lock Does not do any None-Locksmithing Jobs No Garage Door Fix and Install Jobs",
-Rochester L&K,"Company : Rochester L&K (LINE-6) 
+Line 6: Rochester L&K,"Company : Rochester L&K (LINE-6) 
 ",Uleadz Technician: Line 6: Rochester L&K tech,"Metro:  Rochester, NY",Service Area: No Service Area (No OOA Jobs),"Affiliates: None
 ",Hours of Operation: Sunday - Thursday from 7:00 AM – 12:00 AM Friday from 7:00 AM – 4:00 PM Saturday: Closed,"Can't Do List: Ferrari, Bugatti & Pagani Volkswagen & BMW 2007 & Up Mercedes Benz, Audi, Porsche, Volvo & any other German Cars No Motorcycles / Motorbikes",
-Pro-Keys LS ,"Company : PRO-KEYS LOCKSMITH (LINE-7)
+Line 7: Pro-Keys LS ,"Company : PRO-KEYS LOCKSMITH (LINE-7)
 ",Uleadz Technician: Line 7 : Prokeys,"Metro:  Dallas & Fort Worth TX, New York NY
 ",Service Area: No Service Area (No OOA Jobs),Affiliates: Yelp!,Hours of Operation: Sunday - Thursday from 7:00 AM – 1:00 AM Friday from 7:00 AM – 5:00 PM Saturday from 8:00 PM – 1:00 AM,Can't Do List: Saab - Motorcycles - ATVs - Cars Older than 1997 - BMW Older than 2004 - BMW 2017 or Newer - Mercedes Older than 2004 - Mercedes Newer than 2014 - House Key Duplicates,
-Local Services,"Company : PRO-KEYS LOCKSMITH (LINE-7)
+Line 7: Local Services,"Company : PRO-KEYS LOCKSMITH (LINE-7)
 ",Uleadz Technician: Line 7 : Prokeys,"Metro:  Dallas & Fort Worth TX, New York NY
 ",Service Area: No Service Area (No OOA Jobs),Affiliates: Yelp!,Hours of Operation: Sunday - Thursday from 7:00 AM – 1:00 AM Friday from 7:00 AM – 5:00 PM Saturday from 8:00 PM – 1:00 AM,Can't Do List: Saab - Motorcycles - ATVs - Cars Older than 1997 - BMW Older than 2004 - BMW 2017 or Newer - Mercedes Older than 2004 - Mercedes Newer than 2014 - House Key Duplicates,
-Quick Rescue LS,"Company : PRO-KEYS LOCKSMITH (LINE-7)
+Line 7: Quick Rescue LS,"Company : PRO-KEYS LOCKSMITH (LINE-7)
 ",Uleadz Technician: Line 7 : Prokeys,"Metro:  Dallas & Fort Worth TX, New York NY
 ",Service Area: No Service Area (No OOA Jobs),Affiliates: Yelp!,Hours of Operation: Sunday - Thursday from 7:00 AM – 1:00 AM Friday from 7:00 AM – 5:00 PM Saturday from 8:00 PM – 1:00 AM,Can't Do List: Saab - Motorcycles - ATVs - Cars Older than 1997 - BMW Older than 2004 - BMW 2017 or Newer - Mercedes Older than 2004 - Mercedes Newer than 2014 - House Key Duplicates,
-A1 Lock Express ,"Company : A1 LOCK EXPRESS (LINE-8) 
+Line 8: A1 Lock Express ,"Company : A1 LOCK EXPRESS (LINE-8) 
 ","Uleadz Technician: Line 8 : A1 Lock Express Technician
 ","Metro:  Austin, Texas
 ",Service Area: No Service Area (No OOA Jobs),"Affiliates: Home Advisor, Angie's List
 ",Hours of Operation: 24/7 : Open 7 Days a Week,"Can't Do List: Motorcycles / Motorbikes
 ",
-All Keys LS  ,"Company : ALL KEYS LOCKSMITH (LINE-10)
+Line 10: All keys LS  ,"Company : ALL KEYS LOCKSMITH (LINE-10)
 ","Uleadz Technician: Line 10 : All Keys Tech
 ","Metro:  Los Angeles, CA",Service Area: No Service Area (No OOA Jobs),"Affiliates: None
 ",Hours of Operation: Monday - Sunday from 7:00 AM – 12:00 AM Emergency Lockouts: 24/7 : Open 7 Days a Week,Can't Do List: House / Business Key Duplicates No Motorcycles / Motorbikes,
-MT Local LS  ,Company : MT Lock And Key (LINE-9) ,"Uleadz Technician: Line 9: MT Lock And Key
+Line 10: Mt Local LS  ,Company : MT Lock And Key (LINE-9) ,"Uleadz Technician: Line 9: MT Lock And Key
 ","Metro:  Los Angeles, California","Service Area: No Service Area (No OOA Jobs)
 ",Affiliates: None,Hours of Operation: Saturday - Thursday from 7:00 AM – 11:00 PM Friday from 7:00 AM – 7:00 PM Emergency Lockouts: 24/7 : Open 7 Days a Week,Can't Do List: House / Business Key Duplicates ,
-MT Lock and Key  ,Company : MT Lock And Key (LINE-9) ,"Uleadz Technician: Line 9: MT Lock And Key
+Line 9: Mt lock and keys,Company : MT Lock And Key (LINE-9) ,"Uleadz Technician: Line 9: MT Lock And Key
 ","Metro:  Los Angeles, California","Service Area: No Service Area (No OOA Jobs)
 ",Affiliates: None,Hours of Operation: Saturday - Thursday from 7:00 AM – 11:00 PM Friday from 7:00 AM – 7:00 PM Emergency Lockouts: 24/7 : Open 7 Days a Week,Can't Do List: House / Business Key Duplicates ,
-Mobile Locksmith of PGH,"Company : Pittsburgh Mobile LS (LINE-12)
+Line 12: Mobile LS of PGH,"Company : Pittsburgh Mobile LS (LINE-12)
 ",Uleadz Technician: Line 12: Pittsburgh Mobile Tech.,"Metro:  Pittsburgh, PA",Service Area: Service area inside Uleadz,"Affiliates: None
 ","Hours of Operation: 7:30 AM - 11:00 PM : 7 Days a Week
 ","Can't Do List: No Can’t Do List
 ",
-Mobile Locksmith of PGH (Guarantee),"Company : Pittsburgh Mobile LS (LINE-12)
+Line 12: Mobile LS of PGH (Guarantee),"Company : Pittsburgh Mobile LS (LINE-12)
 ",Uleadz Technician: Line 12: Pittsburgh Mobile Tech.,"Metro:  Pittsburgh, PA",Service Area: Service area inside Uleadz,"Affiliates: None
 ","Hours of Operation: 7:30 AM - 11:00 PM : 7 Days a Week
 ","Can't Do List: No Can’t Do List
@@ -131,7 +131,7 @@ const actual = reference.lastElementChild;
 const techtitle = document.querySelector('[title="Job Source"]');
 var techtitletext = techtitle.textContent;
 var trimmedtech = techtitletext.trim();
-console.log(trimmedtech);
+
 const createDiv1 = document.createElement('p');
 const createDiv2 = document.createElement('p');
 const createDiv3 = document.createElement('p');
@@ -145,7 +145,9 @@ const createDiv9 = document.createElement('p');
 
 
 
+
 let arrayobj = [];
+
 
 Papa.parse(csv,{
     
@@ -154,13 +156,19 @@ Papa.parse(csv,{
     },
     complete: function(results) {
         arrayobj = results.data;
+        console.log(arrayobj);
+        
         
              
       
         
         for (var i = 0; i < arrayobj.length; i++) { 
           for (var x = 0; x < arrayobj[i].length; x++) { 
-            if (arrayobj[i][x]===trimmedtech){
+            console.log(arrayobj[40][0]);
+            console.log(trimmedtech);
+        let trimarrobj = arrayobj[i][x].trim()
+            if (trimarrobj===trimmedtech){
+              
              
 
               createDiv1.textContent = arrayobj[i][0];
